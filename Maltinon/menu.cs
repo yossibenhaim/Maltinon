7d08b78add08c3993838a3ffed334673ef42a50a
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-09 16:18:30 +0300

    create a nemu

---
+++ Maltinon/menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZstdSharp.Unsafe;
+
+namespace Maltinon
+{
+    internal class menu
+    {
+        public void startNemu()
+        {
+            bool run = true;
+            int choice;
+
+            while (run)
+            {
+                Console.WriteLine("send a number");
+                string choiceStr = Console.ReadLine();
+
+                while (true)
+                {
+                    try
+                    {
+                        choice = int.Parse(choiceStr);
+                        break;
+                    }
+                    catch { Console.WriteLine("plese send a number!"); }
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
+
+            }
+        }
+    }
+}
