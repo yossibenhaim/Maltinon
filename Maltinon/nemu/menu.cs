63c326888dad6b5047853682ae827dd02e47ab05
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-12 15:59:50 +0300

    cline folders

--- Maltinon/nemu/menu.cs
+++ Maltinon/nemu/menu.cs
@@ -22,14 +22,14 @@ namespace Maltinon
             bool run = true;
             int choice;
             string pseudonym = MenuLogin();
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Entering the menu.", "info", "menu.startMenu"));
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Entering the menu loop.", "info", "menu.startNemu"));
+            Logs.SendLog($"Entering the menu.", "info", "menu.startMenu");
+            Logs.SendLog($"Entering the menu loop.", "info", "menu.startNemu");
             while (run)
             {
                 Prints.ShowMenuLogin();
 
                 string choiceStr;
-                Logs.SendLog(SqlQueryBuilder.ReadLog($"Enters an input checking loop for the menu.", "info", "menu.startNemu"));
+                Logs.SendLog($"Enters an input checking loop for the menu.", "info", "menu.startNemu");
 
                 while (true)
                 {
@@ -42,62 +42,62 @@ namespace Maltinon
                     catch
                     {
                         Prints.ShowRequestEnterNumber();
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Invalid input received.", "warning", "menu.startNemu"));
+                        Logs.SendLog($"Invalid input received.", "warning", "menu.startNemu");
                     }
                 }
-                Logs.SendLog(SqlQueryBuilder.ReadLog($"Input OK - exits the loop.", "info", "menu.startNemu"));
+                Logs.SendLog($"Input OK - exits the loop.", "info", "menu.startNemu");
 
                 switch (choice)
                 {
                     case 1:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Select Add Report.", "info", "menu.startNemu"));
+                        Logs.SendLog($"Select Add Report.", "info", "menu.startNemu");
                         menager.AddRepoert(pseudonym);
                         break;
                     case 2:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Selected Print code name by name.", "info", "menu.startNemu"));
+                        Logs.SendLog($"Selected Print code name by name.", "info", "menu.startNemu");
                         menager.PrintPseudonym();
                         break;
                     case 3:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"User change selected.", "info", "menu.startNemu"));
+                        Logs.SendLog($"User change selected.", "info", "menu.startNemu");
                         pseudonym = MenuLogin();
                         break;
                     case 4:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Exit menu selected.", "info", "menu.startNemu"));
+                        Logs.SendLog($"Exit menu selected.", "info", "menu.startNemu");
                         run = false;
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("✔ Exiting the system. Stay safe!");
                         Console.ResetColor();
                         break;
                     default:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Invalid input received.", "warning", "menu.startNemu"));
+                        Logs.SendLog($"Invalid input received.", "warning", "menu.startNemu");
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("ℹ Please choose a number between 1 and 4.");
                         Console.ResetColor();
                         break;
                 }
             }
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"You have exited the menu loop..", "info", "menu.startNemu"));
+            Logs.SendLog($"You have exited the menu loop..", "info", "menu.startNemu");
         }
         public string MenuLogin()
         {
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Entering the menuLogin.", "info", "menu.MenuLogin"));
+            Logs.SendLog($"Entering the menuLogin.", "info", "menu.MenuLogin");
             Prints.ShowStartUsers();
 
             string pseudonym = Console.ReadLine();
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"User entered a secret code ({pseudonym}).", "info", "menu.MenuLogin"));
+            Logs.SendLog($"User entered a secret code ({pseudonym}).", "info", "menu.MenuLogin");
 
             string status = menager.CheckExistingUser(pseudonym);
             if (status.Length == 0)
             {
-                Logs.SendLog(SqlQueryBuilder.ReadLog($"Code name not found. Goes to search by name ({pseudonym}).", "info", "menu.MenuLogin"));
+                Logs.SendLog($"Code name not found. Goes to search by name ({pseudonym}).", "info", "menu.MenuLogin");
                 return menager.CreatUser();
             }
             else
             {
-                Logs.SendLog(SqlQueryBuilder.ReadLog($"User ({pseudonym}) found. Goes to menu.", "info", "menu.MenuLogin"));
+                Logs.SendLog($"User ({pseudonym}) found. Goes to menu.", "info", "menu.MenuLogin");
                 if (menager.CheckIfAgent(status))
                 {
-                    Logs.SendLog(SqlQueryBuilder.ReadLog($"The user ({pseudonym}) is an agent. Goes to the agents menu.", "info", "menu.MenuLogin"));
+                    Logs.SendLog($"The user ({pseudonym}) is an agent. Goes to the agents menu.", "info", "menu.MenuLogin");
                     menuAgent();
                 }
                 return pseudonym;
@@ -110,14 +110,14 @@ namespace Maltinon
         {
             bool run = true;
             int choice;
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Entering the menu loop.", "info", "menu.MenuAgent"));
+            Logs.SendLog($"Entering the menu loop.", "info", "menu.MenuAgent");
             while (run)
             {
 
                 Prints.ShowMenuAgent();
 
                 string choiceStr = Console.ReadLine();
-                Logs.SendLog(SqlQueryBuilder.ReadLog($"Enters an input checking loop for the menu.", "info", "menu.MenuAgent"));
+                Logs.SendLog($"Enters an input checking loop for the menu.", "info", "menu.MenuAgent");
                 while (true)
                 {
                     try
@@ -128,61 +128,61 @@ namespace Maltinon
                     catch
                     {
                         Prints.ShowRequestEnterNumber();
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Invalid input received.", "warning", "menu.MenuAgent"));
+                        Logs.SendLog($"Invalid input received.", "warning", "menu.MenuAgent");
 
 
                     }
                 }
-                Logs.SendLog(SqlQueryBuilder.ReadLog($"Input OK - exits the loop.", "info", "menu.MenuAgent"));
+                Logs.SendLog($"Input OK - exits the loop.", "info", "menu.MenuAgent");
 
                 switch (choice)
                 {
                     case 1:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Selected Print Reports.", "info", "menu.MenuAgent"));
+                        Logs.SendLog($"Selected Print Reports.", "info", "menu.MenuAgent");
                         menager.PrintResophns();
                         break;
                     case 2:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Selected for printing candidates for recruitment.", "info", "menu.MenuAgent"));
+                        Logs.SendLog($"Selected for printing candidates for recruitment.", "info", "menu.MenuAgent");
                         menager.PrintCandidateEligibility();
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Enter the recruitment candidates menu.", "info", "menu.MenuAgent"));
+                        Logs.SendLog($"Enter the recruitment candidates menu.", "info", "menu.MenuAgent");
                         MenuCandidateEligibility();
                         break;
                     case 3:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Prints dangerous people.", "info", "menu.MenuAgent"));
+                        Logs.SendLog($"Prints dangerous people.", "info", "menu.MenuAgent");
                         menager.PrintDangerousTargets();
                         break;
                     case 4:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Select Print personal code.", "info", "menu.MenuAgent"));
+                        Logs.SendLog($"Select Print personal code.", "info", "menu.MenuAgent");
                         menager.PrintPseudonym();
                         break;
                     case 5:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"User exit selected.", "info", "menu.MenuAgent"));
+                        Logs.SendLog($"User exit selected.", "info", "menu.MenuAgent");
                         run = false;
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("✔ Exiting Agent Mode. Logged out.");
                         Console.ResetColor();
                         break;
                     default:
-                        Logs.SendLog(SqlQueryBuilder.ReadLog($"Invalid input received.", "warning", "menu.MenuAgent"));
+                        Logs.SendLog($"Invalid input received.", "warning", "menu.MenuAgent");
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("ℹ Please choose a number between 1 and 4.");
                         Console.ResetColor();
                         break;
                 }
             }
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"You have exited the menu loop..", "info", "menu.MenuAgent"));
+            Logs.SendLog($"You have exited the menu loop..", "info", "menu.MenuAgent");
 
         }
 
         public void MenuCandidateEligibility()
         {
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Entering MenuCandidateEligibility.", "info", "menu.MenuCandidateEligibility"));
+            Logs.SendLog($"Entering MenuCandidateEligibility.", "info", "menu.MenuCandidateEligibility");
 
             Prints.ShowMenuCandidateEligibility();
 
             int choice;
             string choiceStr = Console.ReadLine();
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Enters an input checking loop for the menu.", "info", "menu.MenuCandidateEligibility"));
+            Logs.SendLog($"Enters an input checking loop for the menu.", "info", "menu.MenuCandidateEligibility");
             while (true)
             {
                 try
@@ -193,31 +193,31 @@ namespace Maltinon
                 catch
                 {
                     Prints.ShowRequestEnterNumber();
-                    Logs.SendLog(SqlQueryBuilder.ReadLog($"Invalid input received.", "warning", "menu.MenuCandidateEligibility"));
+                    Logs.SendLog($"Invalid input received.", "warning", "menu.MenuCandidateEligibility");
 
                 }
             }
-            Logs.SendLog(SqlQueryBuilder.ReadLog($"Input OK - exits the loop.", "info", "menu.MenuCandidateEligibility"));
+            Logs.SendLog($"Input OK - exits the loop.", "info", "menu.MenuCandidateEligibility");
 
 
             switch (choice)
             {
                 case 1:
-                    Logs.SendLog(SqlQueryBuilder.ReadLog($"Elected promotion of all candidates..", "info", "menu.MenuAgent"));
+                    Logs.SendLog($"Elected promotion of all candidates..", "info", "menu.MenuAgent");
                     menager.UpdeteCandidateEligibility();
                     break;
                 case 2:
-                    Logs.SendLog(SqlQueryBuilder.ReadLog($"Selected Promotion of a specific candidate for selection..", "info", "menu.MenuAgent"));
+                    Logs.SendLog($"Selected Promotion of a specific candidate for selection..", "info", "menu.MenuAgent");
                     menager.UpdeteStatusToAgent();
                     break;
                 case 3:
-                    Logs.SendLog(SqlQueryBuilder.ReadLog($"Cancel and exit selected..", "info", "menu.MenuAgent"));
+                    Logs.SendLog($"Cancel and exit selected..", "info", "menu.MenuAgent");
                     Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine("↩ Action cancelled.");
                     Console.ResetColor();
                     break;
                 default:
-                    Logs.SendLog(SqlQueryBuilder.ReadLog($"Invalid input received.", "warning", "menu.MenuCandidateEligibility"));
+                    Logs.SendLog($"Invalid input received.", "warning", "menu.MenuCandidateEligibility");
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("ℹ Please choose a number between 1 and 3.");
                     Console.ResetColor();
